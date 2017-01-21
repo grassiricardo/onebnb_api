462c70e18340de1bf602bee38b878cddd0e51e0f
grassi <ricardograssi91@gmail.com>
2017-01-20 22:14:17 -0200

    add GEOCODER in project

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,18 @@
 class Address < ApplicationRecord
+  # Após a validação de dados chama o método para pegar a latitude e longitude
+  after_validation :geocode
+
+  # Chama a API do geocode e devolve os dados completos do endereço
+  # Com os dados nós preenchemos a latitude e longitude
+  geocoded_by :full_address do |obj,results|
+    if geo = results.first
+      obj.latitude = geo.latitude
+      obj.longitude = geo.longitude
+    end
+  end
+
+  # Monta uma String com o endereço completo
+  def full_address
+    "#{self.street}, #{self.neighborhood}, #{self.city} #{number}, #{self.country}"
+  end
 end
