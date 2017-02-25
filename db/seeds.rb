4c2e12bdcc2f46fcd80e2dad609dc55174e71777
grassi <ricardograssi91@gmail.com>
2017-02-24 23:08:09 -0300

    modification of index.jbuild taks and incremented seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,39 +3,39 @@ a2 = Address.create country: "Brazil", state: "São Paulo", city: "São Paulo", 
 a3 = Address.create country: "Brazil", state: "São Paulo", city: "São José do Rio Pardo"
 a4 = Address.create country: "Brazil", state: "São Paulo", city: "Casa Branca"
 a5 = Address.create country: "Brazil", state: "São Paulo", city: "São Paulo", street: "Av Paulista", number: "1000"
- 
+
 a6 = Address.create country: "Brazil", state: "Minas Gerais", city: "Varginha"
 a7 = Address.create country: "Brazil", state: "Minas Gerais", city: "Belo Horizonte"
 a8 = Address.create country: "Brazil", state: "Minas Gerais", city: "Machado"
 a9 = Address.create country: "Brazil", state: "Minas Gerais", city: "Poços de Caldas"
 a10 = Address.create country: "Brazil", state: "Minas Gerais", city: "Montes Claros"
- 
+
 a11 = Address.create country: "Brazil", state: "Santa Catarina", city: "Garopaba"
 a12 = Address.create country: "Brazil", state: "Santa Catarina", city: "Joenvile"
 a13 = Address.create country: "Brazil", state: "Santa Catarina", city: "Blumenau"
 a14 = Address.create country: "Brazil", state: "Santa Catarina", city: "Florianópolis"
 a15 = Address.create country: "Brazil", state: "Santa Catarina", city: "São José"
- 
+
 a16 = Address.create country: "Brazil", state: "Goiás", city: "Caldas Novas"
 a17 = Address.create country: "Brazil", state: "Goiás", city: "Caldas Novas", street: "Av. Cel. Bento de Godoy"
 a18 = Address.create country: "Brazil", state: "Goiás", city: "Goiania"
 a19 = Address.create country: "Brazil", state: "Goiás", city: "Rio Quente"
 a20 = Address.create country: "Brazil", state: "Goiás", city: "Oloana"
- 
+
 f1 = Facility.create wifi: true, washing_machine: true
 f2 = Facility.create wifi: false, washing_machine: false
- 
+
 u1 = User.create email: "fake@teste.com", password: "12345678", name: "Fake Guy", photo: open("db/seed_images/steve.jpg"), address: a1, confirmed_at: Time.now
 u2 = User.create email: "nobody@teste.com", password: "12345678", name: "Mr Nobody", photo: open("db/seed_images/bill.jpeg"), address: a2, confirmed_at: Time.now
- 
+
 c1 = Comment.create body: 'Hey You Im just a little comment to be a example in this awesome APP', user: u1
 c2 = Comment.create body: 'Hey You Im just a little comment to be a example in this awesome APP', user: u1
 c3 = Comment.create body: 'Hey You Im just a little comment to be a example in this awesome APP', user: u1
 c4 = Comment.create body: 'Olá eu sou apenas um pequeno comentário que serve como um exemplo nesse APP incrível \o/', user: u2
 c5 = Comment.create body: 'Olá eu sou apenas um pequeno comentário que serve como um exemplo nesse APP incrível \o/', user: u2
 c6 = Comment.create body: 'Olá eu sou apenas um pequeno comentário que serve como um exemplo nesse APP incrível \o/', user: u2
- 
- 
+
+
 p1 = Photo.create(photo: open("db/seed_images/p1.jpg"))
 p2 = Photo.create(photo: open("db/seed_images/p2.jpg"))
 p3 = Photo.create(photo: open("db/seed_images/p3.jpg"))
@@ -62,8 +62,8 @@ p23 = Photo.create(photo: open("db/seed_images/p20.jpg"))
 p24 = Photo.create(photo: open("db/seed_images/p18.jpg"))
 p25 = Photo.create(photo: open("db/seed_images/p19.jpg"))
 p26 = Photo.create(photo: open("db/seed_images/p20.jpg"))
- 
- 
+
+
 property = Property.new price: 20, address: a1, facility: f1, user: u1, status: :active, name: "#{a1.city} #{a1.id}", rating: 1, accommodation_type: :whole_house, beds: 2, bedroom: 3, bathroom: 2, guest_max: 5, description: 'É um belo Lugar para se visita'
 [p1, p21, p22, p23].each {|photo| property.photos << photo}
 [c1, c2, c3, c4, c5, c6].each {|comment| property.comments << comment}
@@ -95,7 +95,7 @@ property.save!
 property = Property.new price: 20, address: a10, facility: f1, user: u1, status: :active, name: "#{a10.city} #{a10.id}", rating: 0, accommodation_type: :whole_bedroom, beds: 3, bedroom: 4, bathroom: 3, guest_max: 4, description: 'É um belo Lugar para se visita'
 property.photos << p10
 property.save!
- 
+
 property = Property.new price: 20, address: a11, facility: f2, user: u2, status: :active, name: "#{a11.city} #{a11.id}", rating: 5, accommodation_type: :whole_bedroom, beds: 3, bedroom: 4, bathroom: 3, guest_max: 4, description: 'É um belo Lugar para se visita'
 property.photos << p11
 property.save!
@@ -126,5 +126,23 @@ property.save!
 property = Property.new price: 20, address: a20, facility: f2, user: u2, status: :active, name: "#{a20.city} #{a20.id}", rating: 1, accommodation_type: :shared_bedroom, beds: 4, bedroom: 4, bathroom: 3, guest_max: 4, description: 'É um belo Lugar para se visita'
 property.photos << p20
 property.save!
- 
+
 Property.reindex
+
+r1 = Reservation.create(property: Property.all[10], user: u1, checkin_date: Time.now + 10.days, checkout_date: Time.now + 30.days, status: :active)
+
+t1 = Talk.create(property: Property.all[10], user: u1, reservation: r1)
+
+Message.create(talk: t1, user: u1, body: 'Hey')
+Message.create(talk: t1, user: u2, body: 'Hello')
+
+Message.create(talk: t1, user: u1, body: 'I want rent your property')
+Message.create(talk: t1, user: u2, body: 'Sure, you can get the details in Property Details')
+
+t2 = Talk.create(property: Property.all[0], user: u2)
+
+Message.create(talk: t2, user: u2, body: 'Hey')
+Message.create(talk: t2, user: u1, body: 'Hello')
+
+Message.create(talk: t2, user: u2, body: 'I want rent your property')
+Message.create(talk: t2, user: u1, body: 'Sure, you can get the details in Property Details')
